b8cd28c1d57042202f4f76082850cf50f8a804a0
maplejaw <i@maplejaw.com>
2021-12-11 09:56:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,3 @@
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
-# 修改默认登录密码
-sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$EVQNUXgW$MBP1fj3byNZHdT63eYpig/:18972:0:99999:7:::/g' package/base-files/files/etc/shadow
